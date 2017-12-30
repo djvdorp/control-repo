02ad9f995bc957678db2361cb837ab98e3fbf728
Daniel van Dorp <daniel.van.dorp@mendix.com>
2017-12-30 18:41:20 +0100

    Add get_iplayer directories and cron

--- site-modules/profile/manifests/get_iplayer.pp
+++ site-modules/profile/manifests/get_iplayer.pp
@@ -8,12 +8,33 @@ class profile::get_iplayer {
         'libxml-libxml-perl',
         'libcgi-pm-perl',
         'atomicparsley',
-        'ffmpeg'
+        'ffmpeg',
     ])
 
     file { '/usr/local/bin/get_iplayer':
         ensure  => file,
         mode    => '0755',
         source  => 'https://raw.github.com/get-iplayer/get_iplayer/master/get_iplayer',
+    }
+
+    file { '/home/daniel/bbcrips/':
+        ensure  => directory,
+        owner   => 'daniel',
+        group   => 'daniel',
+    }
+
+    file { '/home/daniel/logs/':
+        ensure  => directory,
+        owner   => 'daniel',
+        group   => 'daniel',
+    }
+
+    cron { 'get_iplayer':
+        user    => 'daniel',
+        ensure  => present,
+        command => '/usr/local/bin/get_iplayer --type=radio --modes=flashaudio,iphone,flashaac,realaudio --output="/home/daniel/bbcrips/" --pvr 2>&1>>/home/daniel/logs/get-iplayer.log',
+        minute  => '40',
+        hour    => '*',
+        require => [ File['/usr/local/bin/get_iplayer'], File['/home/daniel/bbcrips/'], File['/home/daniel/logs/'] ],
     }
 }
