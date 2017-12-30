c14a2046f16d389f5e72e9d111c0f33e0bde01c2
Daniel van Dorp <daniel.van.dorp@mendix.com>
2017-12-30 21:15:35 +0100

    Add flexget profile to mediaserver

--- site-modules/profile/manifests/flexget.pp
+++ site-modules/profile/manifests/flexget.pp
@@ -1,2 +1,47 @@
 class profile::flexget {
+    ensure_packages([
+    #    'virtualenv',
+    #    'python-virtualenv',
+        'python3-virtualenv',
+    ])
+
+    class { 'python' :
+        version    => 'system',
+        pip        => 'present',
+        dev        => 'absent',
+        virtualenv => 'present',
+        gunicorn   => 'absent',
+    }
+
+    python::virtualenv { '/home/daniel/flexget' :
+        ensure       => present,
+        version      => 'system',
+        systempkgs   => true,
+        distribute   => true,
+        owner        => 'daniel',
+        group        => 'daniel',
+    }
+
+    python::pip { 'flexget' :
+        pkgname       => 'flexget',
+        ensure        => 'present',
+        virtualenv    => '/home/daniel/flexget',
+        owner         => 'daniel',
+    }
+
+    python::pip { 'transmissionrpc' :
+        pkgname       => 'transmissionrpc',
+        ensure        => 'present',
+        virtualenv    => '/home/daniel/flexget',
+        owner         => 'daniel',
+    }
+
+    cron { 'flexget':
+        user    => 'daniel',
+        ensure  => present,
+        command => 'source /home/daniel/flexget/bin/activate; /home/daniel/flexget/bin/flexget --cron --loglevel debug --logfile /home/daniel/logs/flexget.log execute 2>&1>>/home/daniel/logs/flexget.log',
+        minute  => '20',
+        hour    => '*',
+        require => Class['python'],
+    }
 }
