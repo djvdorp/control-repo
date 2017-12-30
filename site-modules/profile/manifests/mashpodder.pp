c8cc55c18d5d3c3beea49d2a95cd294762b6c21a
Daniel van Dorp <daniel.van.dorp@mendix.com>
2017-12-30 20:31:32 +0100

    Add mashpodder profile to mediaserver

--- site-modules/profile/manifests/mashpodder.pp
+++ site-modules/profile/manifests/mashpodder.pp
@@ -1,2 +1,26 @@
 class profile::mashpodder {
+    file { '/home/daniel/mashpodder/':
+        ensure  => directory,
+        owner   => 'daniel',
+        group   => 'daniel',
+    }
+
+    vcsrepo { '/home/daniel/mashpodder/':
+        ensure   => present,
+        provider => 'git',
+        source   => 'https://github.com/chessgriffin/mashpodder.git',
+        user     => 'daniel',
+        group    => 'daniel',
+        revision => 'master',
+        require  => [ Package['git'], File['/home/daniel/mashpodder/'] ],
+    }
+
+    cron { 'mashpodder':
+        user    => 'daniel',
+        ensure  => present,
+        command => '/home/daniel/mashpodder/mashpodder.sh 2>&1>>/home/daniel/logs/mashpodder.log',
+        minute  => '0',
+        hour    => '*',
+        require => File['/home/daniel/mashpodder/'],
+    }
 }
