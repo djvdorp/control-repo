2dab39bbcb591703a8df2b25d0d7c74a202d8d2c
Daniel van Dorp <daniel@vandorp.biz>
2019-10-11 15:22:32 +0200

    DO: tidy up puppet reports dir, stop filling disk

--- site-modules/profile/manifests/puppet.pp
+++ site-modules/profile/manifests/puppet.pp
@@ -2,6 +2,16 @@
 class profile::puppet (
     Boolean $autorun_puppet = true,
 ) {
+  file { '/usr/local/bin/run-puppet':
+    source => 'puppet:///modules/profile/puppet/run-puppet.sh',
+    mode   => '0755',
+  }
+
+  file { '/usr/local/bin/papply':
+    source => 'puppet:///modules/profile/puppet/papply.sh',
+    mode   => '0755',
+  }
+
   if $autorun_puppet {
       service { ['puppet', 'mcollective', 'pxp-agent']:
         ensure => stopped, # Puppet runs from cron
@@ -15,19 +25,17 @@ class profile::puppet (
         hour    => '*',
       }
 
-      file { '/usr/local/bin/run-puppet':
-        source => 'puppet:///modules/profile/puppet/run-puppet.sh',
-        mode   => '0755',
-      }
-
-      file { '/usr/local/bin/papply':
-        source => 'puppet:///modules/profile/puppet/papply.sh',
-        mode   => '0755',
-      }
-
       file { '/tmp/puppet.lastrun':
         content => strftime('%F %T'),
         backup  => false,
       }
+
+      tidy { '/opt/puppetlabs/puppet/cache/reports':
+        age     => '30d',
+        matches => '*.yaml',
+        recurse => true,
+        rmdirs  => false,
+        type    => mtime,
+    }
   }
 }
