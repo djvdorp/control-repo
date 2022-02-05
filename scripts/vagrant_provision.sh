0a025e83bef439d036b0b595a41381ab6aec8eca
Daniel van Dorp <daniel@vandorp.biz>
2022-02-05 18:34:35 +0100

    Sync with upstream puppet code

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -4,7 +4,7 @@ apt-key adv --fetch-keys http://apt.puppetlabs.com/DEB-GPG-KEY-puppet
 wget http://apt.puppetlabs.com/puppetlabs-release-pc1-${DISTRIB_CODENAME}.deb
 dpkg -i puppetlabs-release-pc1-${DISTRIB_CODENAME}.deb
 apt-get update
-apt-get -y install puppet-agent
+apt-get -y install git puppet-agent
 /opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri
 cd /etc/puppetlabs/code/environments/production
 /opt/puppetlabs/puppet/bin/r10k puppetfile install --verbose
