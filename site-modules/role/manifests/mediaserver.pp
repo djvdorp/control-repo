c46e928543184751b8291446becc7041144fdebf
Daniel van Dorp <daniel@vandorp.biz>
2022-04-23 10:40:18 +0200

    2022 reinstall: minimalism

--- site-modules/role/manifests/mediaserver.pp
+++ site-modules/role/manifests/mediaserver.pp
@@ -1,9 +1,9 @@
 class role::mediaserver {
   include profile::get_iplayer
   include profile::bittorrent
-  include profile::flexget
-  include profile::mashpodder
+#  include profile::flexget
+#  include profile::mashpodder
   include profile::packtpub
-  include profile::plexmediaserver
+#  include profile::plexmediaserver
   include profile::nginx
 }
