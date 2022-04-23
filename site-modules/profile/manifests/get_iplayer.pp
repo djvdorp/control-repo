74ef3d121f5cafcaa5d565c46e7110ca70fe9e81
Daniel van Dorp <daniel@vandorp.biz>
2022-04-23 10:35:30 +0200

    2022 reinstall: get_iplayer

--- site-modules/profile/manifests/get_iplayer.pp
+++ site-modules/profile/manifests/get_iplayer.pp
@@ -1,7 +1,7 @@
 include apt
 
 class profile::get_iplayer {
-    apt::ppa { 'ppa:jon-hedgerows/get-iplayer': }
+    apt::ppa { 'ppa:m-grant-prg/utils': }
 
     ensure_packages(
         [
@@ -36,18 +36,18 @@ class profile::get_iplayer {
     cron { 'get_iplayer':
         user    => 'daniel',
         ensure  => present,
-        command => '/usr/bin/get-iplayer --type=radio --modes=flashaudio,iphone,flashaac,realaudio --output="/home/daniel/bbcrips/" --pvr 2>&1>>/home/daniel/logs/get-iplayer.log',
-        minute  => '40',
-        hour    => '*',
+        command => '/usr/bin/get_iplayer --pvr --quiet >> /home/daniel/logs/get_iplayer.log 2>&1',
+        minute  => '0',
+        hour    => '7',
         require => [ Package['get-iplayer'], File['/home/daniel/bbcrips/'], File['/home/daniel/logs/'] ],
     }
 
     cron { 'cleanup_bbcrips':
         user    => 'daniel',
         ensure  => present,
-        command => 'cd /home/daniel/bbcrips/; rm Bobby_Friction_-*',
-        minute  => '58',
-        hour    => '*',
+        command => 'cd /home/daniel/bbcrips/; rm Asian_Networks_Mixtape_Series_with_Bobby_Friction_-_*',
+        minute  => '0',
+        hour    => '8',
         require => File['/home/daniel/bbcrips/'],
     }
 }
